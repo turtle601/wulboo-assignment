28635ac68206e724dea41c2e64c9f5f4bb513467
turtle601 <turtle601@naver.com>
2025-10-21 17:57:24 +0900

    ♻️ refactor: filter params 수정으로 인한 getInfiniteClass entities 로직 수정

--- src/entities/class/class.query.ts
+++ src/entities/class/class.query.ts
@@ -1,4 +1,7 @@
-import { infiniteQueryOptions as tanstackInfiniteQueryOptions } from '@tanstack/react-query';
+import {
+  infiniteQueryOptions as tanstackInfiniteQueryOptions,
+  queryOptions as tsqQueryOptions,
+} from '@tanstack/react-query';
 import { ClassListType } from '~/src/mocks/storage';
 import { requestAPI } from '~/src/shared/api/request';
 
@@ -16,15 +19,19 @@ export interface ClassListResponse {
 
 export const classQueries = {
   keys: () => ['classes'],
-  lists: (params: ClassListParams) => [...classQueries.keys(), { ...params }],
-  list: (params: ClassListParams) => {
+
+  lists: () => [...classQueries.keys()],
+  enroll: () => [...classQueries.keys(), 'enroll'],
+  created: () => [...classQueries.keys(), 'myCreated'],
+
+  list: (params: string) => {
     return tanstackInfiniteQueryOptions({
-      queryKey: [...classQueries.lists(params)],
+      queryKey: [...classQueries.lists(), { filter: params }],
       queryFn: async ({ pageParam }) => {
         return await requestAPI<ClassListResponse>({
           url: '/classList',
           params: new URLSearchParams({
-            ...params,
+            ...(params && { filter: params }),
             ...(pageParam?.cursor && { cursor: pageParam.cursor }),
             limit: '7',
           }),
@@ -33,9 +40,39 @@ export const classQueries = {
           },
         });
       },
-      initialPageParam: { cursor: '1' },
+      initialPageParam: { cursor: '' },
       getNextPageParam: (lastPage: ClassListResponse) => {
         return lastPage.hasMore ? { cursor: lastPage.nextCursor } : undefined;
+      },
+    });
+  },
+
+  myEnrolledList: () => {
+    return tsqQueryOptions({
+      queryKey: [...classQueries.enroll()],
+      queryFn: async () => {
+        return await requestAPI<ClassListType>({
+          url: '/classes/enroll',
+          options: {
+            method: 'GET',
+            credentials: 'include',
+          },
+        });
+      },
+    });
+  },
+
+  myCreatedList: () => {
+    return tsqQueryOptions({
+      queryKey: [...classQueries.created()],
+      queryFn: async () => {
+        return await requestAPI<ClassListType | { message: string }>({
+          url: '/classes/myCreated',
+          options: {
+            method: 'GET',
+            credentials: 'include',
+          },
+        });
       },
     });
   },
