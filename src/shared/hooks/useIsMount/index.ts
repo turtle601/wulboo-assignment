5f0466d3086be527fd527537a0df866c4a9b303d
turtle601 <turtle601@naver.com>
2025-10-24 15:03:14 +0900

    ✨ feat: useIsMount 커스텀 훅 구현

--- src/shared/hooks/useIsMount/index.ts
+++ src/shared/hooks/useIsMount/index.ts
@@ -1,11 +1,1 @@
-import { useEffect, useState } from 'react';
-
-export const useIsMount = () => {
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  return { isMounted };
-};
+export * from './useIsMount';
