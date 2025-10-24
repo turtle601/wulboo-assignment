1c8673fb98d30e4c8d782ff3a8558152706b3196
turtle601 <turtle601@naver.com>
2025-10-24 15:31:11 +0900

    ♻️ refactor: 레이아웃 개선

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import { Header } from '~/src/widgets/header';
 
 import './globals.css';
 
@@ -33,7 +34,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <QueryClientProvider>
-          <CommonLayout>{children}</CommonLayout>
+          <CommonLayout>
+            <Header />
+            {children}
+          </CommonLayout>
           <Modal.Root />
         </QueryClientProvider>
         <div id="modal-root"></div>
