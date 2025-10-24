e2312277c4489cd6090c91a65e1a9c3e8c4ccb57
turtle601 <turtle601@naver.com>
2025-10-24 14:56:43 +0900

    📦 chore: package.json 및 설정파일 최신화

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:9090/api/:path*', // Express 서버로 중계
+      },
+    ];
+  },
 };
 
 export default nextConfig;
