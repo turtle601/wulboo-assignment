188a1837842aac90045daaf8e12b3bb0659d0750
turtle601 <turtle601@naver.com>
2025-10-24 15:23:10 +0900

    ✨ feat: 강좌개설 관련 기능 구현

--- app/courses/create/page.tsx
+++ app/courses/create/page.tsx
@@ -1,9 +1,28 @@
-import { CreateClass } from '~/src/pages/createClass';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+import { CreateCourses } from '~/src/pages/createCourses';
 
-export default function CreateClassPage() {
+import { API_BASE_URL } from '~/src/shared/api';
+
+export default async function CreateClassPage() {
+  const cookieStore = await cookies();
+  const authToken = cookieStore.get('authToken');
+
+  const userResponse = await fetch(`${API_BASE_URL}/user`, {
+    method: 'GET',
+    headers: {
+      Cookie: `authToken=${authToken?.value || ''}`,
+    },
+  });
+
+  if (!userResponse.ok) {
+    redirect('/');
+    return null;
+  }
+
   return (
     <>
-      <CreateClass />
+      <CreateCourses />
     </>
   );
 }
