e5cc966fff44e568df10644686e871a13b8fe042
turtle601 <turtle601@naver.com>
2025-10-24 15:09:49 +0900

    ✨ feat: commonLayout 스타일 변경

--- src/widgets/commonLayout/commonLayout.tsx
+++ src/widgets/commonLayout/commonLayout.tsx
@@ -2,8 +2,8 @@ export function CommonLayout({ children }: { children: React.ReactNode }) {
   return (
     <main className="w-full min-w-[320px] max-w-[2560px] mx-auto isolate">
       <div className="relative flex min-h-[100dvh] items-center justify-center flex-col bg-neutral-600 px-4 sm:px-6 lg:px-8">
-        <div className="container flex flex-col bg-white w-full min-h-[600px] rounded-2xl py-8 px-6 min-w-[478px] max-w-[768px]">
-          <div className="flex flex-col">{children}</div>
+        <div className="container flex flex-col bg-white w-full h-[600px] rounded-2xl py-8 px-6 min-w-[478px] max-w-[768px]">
+          <div className="w-full h-full overflow-y-auto">{children}</div>
         </div>
       </div>
     </main>
